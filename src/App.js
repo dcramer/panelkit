1d1af277d566daa0ec92d35ce1b25575c302fa33
David Cramer <dcramer@gmail.com>
2020-05-11 17:44:31 -0700

    Move config into index

--- src/App.js
+++ src/App.js
@@ -3,111 +3,9 @@ import PropTypes from "prop-types";
 import { Grid, Cell } from "styled-css-grid";
 import styled from "styled-components";
 
-import { mdiPiHole, mdiLightbulbGroupOff } from "@mdi/js";
-
 import HomeAssistant from "./hass";
 
 import Header from "./components/Header";
-import { TILE } from "./components/tiles";
-
-const config = {
-  tiles: [
-    {
-      width: 2,
-      tiles: [
-        {
-          type: TILE.DOOR_CONTROL,
-        },
-        {
-          type: TILE.ALARM,
-          entityId: "alarm_control_panel.home",
-        },
-        {
-          type: TILE.SWITCH,
-          entityId: "switch.pi_hole",
-          icon: mdiPiHole,
-        },
-      ],
-    },
-    {
-      width: 2,
-      tiles: [
-        {
-          width: 2,
-          type: TILE.SCENE,
-          entityId: "scene.lights_off",
-          name: "Turn Off Lights",
-          icon: mdiLightbulbGroupOff,
-        },
-        {
-          type: TILE.LIGHT,
-          entityId: "light.master_bedroom_lights",
-          name: "Master Bd",
-        },
-        {
-          type: TILE.LIGHT,
-          entityId: "light.guest_bedroom_lights",
-          name: "Guest Bd",
-        },
-        {
-          type: TILE.LIGHT,
-          entityId: "light.hallway_lights",
-          name: "Hallway",
-        },
-        {
-          type: TILE.LIGHT,
-          entityId: "light.office_lights",
-          name: "Office",
-        },
-        {
-          type: TILE.LIGHT,
-          entityId: "light.dining_room_lights",
-          name: "Dining",
-        },
-        {
-          type: TILE.LIGHT,
-          entityId: "light.kitchen_lights",
-          name: "Kitchen",
-        },
-        {
-          type: TILE.LIGHT,
-          entityId: "light.entryway_lights",
-          name: "Entryway",
-        },
-        {
-          type: TILE.LIGHT,
-          entityId: "light.front_door_lights",
-          name: "Front Door",
-        },
-      ],
-    },
-    {
-      width: 4,
-      tiles: [
-        {
-          width: 4,
-          type: TILE.CAMERA,
-          entityId: "camera.front_door_exterior",
-        },
-        {
-          width: 4,
-          type: TILE.CAMERA,
-          entityId: "camera.garage_exterior",
-        },
-        {
-          width: 4,
-          type: TILE.CAMERA,
-          entityId: "camera.garage",
-        },
-        {
-          width: 4,
-          type: TILE.CAMERA,
-          entityId: "camera.backyard",
-        },
-      ],
-    },
-  ],
-};
 
 const Container = styled.div`
   padding: 20px;
@@ -117,14 +15,21 @@ export default class App extends Component {
   static propTypes = {
     url: PropTypes.string,
     accessToken: PropTypes.string,
-    config: PropTypes.object.isRequired,
+    config: PropTypes.shape({
+      tiles: PropTypes.arrayOf(
+        PropTypes.shape({
+          type: PropTypes.elementType,
+          name: PropTypes.string,
+          tiles: PropTypes.array,
+        })
+      ).isRequired,
+    }).isRequired,
     gridWidth: PropTypes.number.isRequired,
   };
 
   static defaultProps = {
     url: process.env.REACT_APP_HASS_URL || "http://localhost:8123",
     accessToken: process.env.REACT_APP_HASS_ACCESS_TOKEN,
-    config,
     gridWidth: 8,
   };
 
@@ -205,7 +110,7 @@ export default class App extends Component {
     return (
       <Container>
         <Header />
-        {this.renderTiles(config.tiles, this.props.gridWidth)}
+        {this.renderTiles(this.props.config.tiles, this.props.gridWidth)}
       </Container>
     );
   }
--- src/index.js
+++ src/index.js
@@ -7,6 +7,109 @@ import Modal from "react-modal";
 
 import "./index.css";
 
+import { TILE } from "./components/tiles";
+// TODO(dcramer): how do we define icons without importing things?
+import { mdiPiHole, mdiLightbulbGroupOff } from "@mdi/js";
+
+const config = {
+  tiles: [
+    {
+      width: 2,
+      tiles: [
+        {
+          type: TILE.DOOR_CONTROL,
+        },
+        {
+          type: TILE.ALARM,
+          entityId: "alarm_control_panel.home",
+        },
+        {
+          type: TILE.SWITCH,
+          entityId: "switch.pi_hole",
+          icon: mdiPiHole,
+        },
+      ],
+    },
+    {
+      width: 2,
+      tiles: [
+        {
+          width: 2,
+          type: TILE.SCENE,
+          entityId: "scene.lights_off",
+          name: "Turn Off Lights",
+          icon: mdiLightbulbGroupOff,
+        },
+        {
+          type: TILE.LIGHT,
+          entityId: "light.master_bedroom_lights",
+          name: "Master Bd",
+        },
+        {
+          type: TILE.LIGHT,
+          entityId: "light.guest_bedroom_lights",
+          name: "Guest Bd",
+        },
+        {
+          type: TILE.LIGHT,
+          entityId: "light.hallway_lights",
+          name: "Hallway",
+        },
+        {
+          type: TILE.LIGHT,
+          entityId: "light.office_lights",
+          name: "Office",
+        },
+        {
+          type: TILE.LIGHT,
+          entityId: "light.dining_room_lights",
+          name: "Dining",
+        },
+        {
+          type: TILE.LIGHT,
+          entityId: "light.kitchen_lights",
+          name: "Kitchen",
+        },
+        {
+          type: TILE.LIGHT,
+          entityId: "light.entryway_lights",
+          name: "Entryway",
+        },
+        {
+          type: TILE.LIGHT,
+          entityId: "light.front_door_lights",
+          name: "Front Door",
+        },
+      ],
+    },
+    {
+      width: 4,
+      tiles: [
+        {
+          width: 4,
+          type: TILE.CAMERA,
+          entityId: "camera.front_door_exterior",
+        },
+        {
+          width: 4,
+          type: TILE.CAMERA,
+          entityId: "camera.garage_exterior",
+        },
+        {
+          width: 4,
+          type: TILE.CAMERA,
+          entityId: "camera.garage",
+        },
+        {
+          width: 4,
+          type: TILE.CAMERA,
+          entityId: "camera.backyard",
+        },
+      ],
+    },
+  ],
+};
+
 WebFont.load({
   google: {
     families: ["Rubik:300,500"],
@@ -17,7 +120,7 @@ Modal.setAppElement("#root");
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App config={config} />
   </React.StrictMode>,
   document.getElementById("root")
 );
