3081350dd7dfac46ed5bae14f603e632cd4908d1
David Cramer <dcramer@gmail.com>
2020-05-13 17:26:27 -0700

    Missing file

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+export const toTitleCase = (str) => {
+  return str
+    .replace(
+      /\w\S*/g,
+      (txt) => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+    )
+    .replace("_", " ");
+};
