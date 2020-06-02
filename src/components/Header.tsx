9209ac8927e137ccffe7a85a2eb051eacd46363b
David Cramer <dcramer@gmail.com>
2020-06-02 11:50:59 -0700

    Add functional datetime to header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { Component } from "react";
+import moment from "moment";
 import styled from "styled-components";
 
 const Header = styled.div`
@@ -11,29 +12,49 @@ const Header = styled.div`
   align-items: flex-end;
 `;
 
-const DateTime = styled.div``;
-
-const Clock = styled.div``;
-
-const Date = styled.div`
+const DateContainer = styled.div`
   font-size: 16pt;
 `;
 
-const Colon = styled.span`
-  animation: blink 2s step-start 0s infinite;
-  font-size: 0.93em;
-  margin: 0 3px;
-`;
+type DateTimeProps = {};
+
+class DateTime<DateTimeProps> extends Component {
+  readonly state: {
+    time: number | null;
+  } = {
+    time: null,
+  };
+
+  timer: number | null = null;
+
+  componentDidMount() {
+    this.setState({ time: new Date().getTime() });
+    this.timer = setTimeout(() => {
+      this.setState({ time: new Date().getTime() });
+    }, 60000);
+  }
+
+  componentWillUnmount() {
+    if (this.timer) clearTimeout(this.timer);
+  }
+
+  render() {
+    if (!this.state.time) return null;
+    return (
+      <div {...this.props}>
+        <div>{moment(this.state.time).format("LT")}</div>
+        <DateContainer>
+          {moment(this.state.time).format("dddd, MMMM Do")}
+        </DateContainer>
+      </div>
+    );
+  }
+}
 
 export default ({ ...props }) => {
   return (
     <Header {...props}>
-      <DateTime>
-        <Clock>
-          14<Colon>:</Colon>54
-        </Clock>
-        <Date>Monday, May 11</Date>
-      </DateTime>
+      <DateTime />
     </Header>
   );
 };
